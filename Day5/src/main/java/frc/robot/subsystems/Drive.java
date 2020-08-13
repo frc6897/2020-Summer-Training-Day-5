b565e29d652334146da27a7265960e64b8c9298e
redt1979 <advik360@gmail.com>
2020-08-13 17:08:44 -0400

    made driveBase

--- Day5/src/main/java/frc/robot/subsystems/Drive.java
+++ Day5/src/main/java/frc/robot/subsystems/Drive.java
@@ -13,35 +13,35 @@ import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drive extends SubsystemBase {
   /**
    * Creates a new Drive.
    */
   private CANSparkMax motorA = new CANSparkMax(0, MotorType.kBrushless);
-  
-  
-  private CANSparkMax motorY = new CANSparkMax(2, MotorType.kBrushless);
-  
+  private CANSparkMax motorZ = new CANSparkMax(2, MotorType.kBrushless);
   
   private CANEncoder encoder1 = new CANEncoder(motorA);
-  private CANEncoder encoder2 = new CANEncoder(motorY);
+  private CANEncoder encoder2 = new CANEncoder(motorZ);
+
+  private DifferentialDrive driverBase = new DifferentialDrive(motorA, motorZ);
 
   
   public Drive() {
     motorA = new CANSparkMax(0, MotorType.kBrushless);
     
-    motorY = new CANSparkMax(2, MotorType.kBrushless);
+    motorZ = new CANSparkMax(2, MotorType.kBrushless);
     
     encoder1 = new CANEncoder(motorA);
-    encoder2 = new CANEncoder(motorY);
+    encoder2 = new CANEncoder(motorZ);
 
   }
 
   //while loop is used to minimize delay
   public void goTenFeet() {
     while(encoder1.getPosition()*1024<(120/2*Math.PI*5)*1024 && encoder2.getPosition()*1024<(120/2*Math.PI*5)*1024) {    //constant doesn't work
-
+    driverBase.tankDrive(1,1);
     }
 
   }
