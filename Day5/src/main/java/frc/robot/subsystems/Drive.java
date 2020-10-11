8d2255caa029feabe96fcb941cb4f79e3eec436b
doggo42 <38188031+doggo4242@users.noreply.github.com>
2020-10-10 20:16:02 -0400

    Update Drive.java

--- Day5/src/main/java/frc/robot/subsystems/Drive.java
+++ Day5/src/main/java/frc/robot/subsystems/Drive.java
@@ -5,12 +5,14 @@ import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Drive extends SubsystemBase{
 	private CANSparkMax[] motors = new CANSparkMax[6];//west coast drivebase, 6 motors
 	private CANEncoder[] enc = new CANEncoder[6];
 	private Ultrasonic us = new Ultrasonic(Constants.US_PORT_1,Constants.US_PORT_2);
 	private int encCt;
+	private DifferentialDrive frontDrive;
 	public Drive(int distance){
 		dist.setAutomaticMode(true);
 		encCt = (12*distance)/(2*Constants.RD*Math.PI)*enc.getCountsPerRevolution();
@@ -19,17 +21,16 @@ public class Drive extends SubsystemBase{
 			motors[i] = new CANSparkMax(Constants.MOTOR_PORT[i],CANSparkMaxLowLevel.MotorType.kBrushless);
 			enc[i] = new CANEncoder(motors[i]);
 		}
+		frontDrive = new DifferentialDrive(motors[Constants.FLMOTOR],motors[Constants.FRMOTOR]);
 	}
 	public void run(){
 		enc.setPosition(0);
 		while(enc[Constants.fLMotor].getPosition()<encCt)
 		{
-			motors[Constants.FLMOTOR].set(1);
-			motors[Constants.FRMOTOR].set(1);
+			frontDrive.tankDrive(1,1);
 		}
 	}
 	public void stop(){
-		motors[Constants.FLMOTOR].set(0);
-		motors[Constants.FRMOTOR].set(0);
+		frontDrive.tankDrive(0,0);
 	}
 }
